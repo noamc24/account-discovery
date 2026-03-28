87f332e598ac943a7e97c1008e1bf01cb4740eb9
noamc24 <noamc24@gmail.com>
2026-03-28 21:12:16 +0300

    Add frontend registration flow

--- front/src/routes.tsx
+++ front/src/routes.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import DashboardPage from "./pages/DashboardPage";
+import RegisterPage from "./pages/RegisterPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
   },
   {
     path: "/dashboard",
